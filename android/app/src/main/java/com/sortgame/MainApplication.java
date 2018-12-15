4bd8f7abac3bf17c27a8f471e5856a2e38755b8f
Stanislav Shakirov <punksta@protonmail.com>
2018-12-15 20:31:02 +0300

    remove reanimated package

--- android/app/src/main/java/com/sortgame/MainApplication.java
+++ android/app/src/main/java/com/sortgame/MainApplication.java
@@ -4,7 +4,6 @@ import android.app.Application;
 
 import com.facebook.react.ReactApplication;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
-import com.swmansion.reanimated.ReanimatedPackage;
 import com.BV.LinearGradient.LinearGradientPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -27,7 +26,6 @@ public class MainApplication extends Application implements ReactApplication {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
             new RNGestureHandlerPackage(),
-            new ReanimatedPackage(),
             new LinearGradientPackage()
       );
     }
