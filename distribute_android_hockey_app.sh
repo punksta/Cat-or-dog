6abbd0ae09766a6b6741028721df812cd7f03e84
Stanislav Shakirov <punksta@protonmail.com>
2019-05-04 21:16:13 +0300

    fix account name

--- distribute_android_hockey_app.sh
+++ distribute_android_hockey_app.sh
@@ -2,6 +2,6 @@
 appcenter distribute release \
     -f "android/app/build/outputs/apk/release/app-release.apk" \
     -r "automatic build '$TRAVIS_BUILD_WEB_URL'" \
-    -a punksta-protonmail.com/Cat-or-dog \
+    -a stanislav-shakirov/Cat-or-dog \
     -g public \
     --disable-telemetry
